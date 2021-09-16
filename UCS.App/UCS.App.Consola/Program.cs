45f39931c4e176f69de9f3567fcd3c36db712892
Angelik3020 <angelica.munoz3021@gmail.com>
2021-09-16 17:22:01 -0500

    creacion proyecto

---
+++ UCS.App/UCS.App.Consola/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UCS.App.Dominio;
+namespace UCS.App.Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Este es mi primer sprit");
+        }
+    }
+}
